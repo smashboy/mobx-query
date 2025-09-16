96e892b61d3c33e73b9c9d711890e554fbce1bf1
SmashBoy <sergeitchaban@gmail.com>
2025-09-16 18:32:17 +0300

    react query dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./libs/react-query.ts";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
 );
